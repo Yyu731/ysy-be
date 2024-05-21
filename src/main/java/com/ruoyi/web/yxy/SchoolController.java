9d430b388c57ff9f3b67ca69273b1b06ea85dade
Yuuang <Yuuang@yy.com>
2024-05-21 13:36:47 +0800

    BriefSchool 修改

--- src/main/java/com/ruoyi/web/yxy/SchoolController.java
+++ src/main/java/com/ruoyi/web/yxy/SchoolController.java
@@ -1,5 +1,6 @@
 package com.ruoyi.web.yxy;
 
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.page.TableDataInfo;
@@ -12,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -21,28 +21,22 @@ public class SchoolController extends BaseController {
 
     @Autowired
     private SchoolInfoService schoolInfoService;
+
     @GetMapping("/hotlist")
-    public TableDataInfo hotlist()
-    {
+    public TableDataInfo hotlist() {
         Page<SchoolInfo> page = getPage();
         System.out.println(page.toString());
-        schoolInfoService.page(page);
-        //10条记录里
+        schoolInfoService.page(page, Wrappers.lambdaQuery(SchoolInfo.class)
+                .eq(SchoolInfo::getSchoolHot, 1));
+
         List<SchoolInfo> records = page.getRecords();
 
-        int num=0;
-        for (SchoolInfo schoolInfo:records){
-            num++;
-            System.out.println("aaa"+num);
-            System.out.println(schoolInfo.getSchoolName());
-
-        }
         List<BriefSchoolVo> briefSchoolVoList = schoolInfoService.getBriefSchoolList(records);
         return getDataTable(briefSchoolVoList);
     }
+
     @GetMapping("/totallist")
-    public TableDataInfo totallist()
-    {
+    public TableDataInfo totallist() {
         Page<SchoolInfo> page = getPage();
         System.out.println(page.toString());
         schoolInfoService.page(page);
